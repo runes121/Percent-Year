fbb4b621aa76f4c6683b95e382018bcc7be50754
runes121 <75335579+runes121@users.noreply.github.com>
2023-04-16 12:07:59 +0100

    - Added progress bar animation - Added styling using ttkbootstrap

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
 from datetime import datetime
 import math
 import tkinter as tk
-from tkinter import ttk
+import ttkbootstrap as ttk
 
-window = tk.Tk()
-window.geometry("700x80")
+window = ttk.Window(themename="darkly")
+window.geometry("700x100")
 window.title("Year progress.")
 window.resizable(False, False)
 
 progress_bar = ttk.Progressbar(window, orient="horizontal", length=500, mode="determinate")
 progress_bar.pack()
+progress_bar.place(relx=0.5, rely=0.2, anchor="center")
 
 today = datetime.now().date()
 day_of_year = today.timetuple().tm_yday
@@ -23,19 +24,25 @@ def calculate():
     print(str(math.floor((day_of_year / 365) * 100)) + f"% of {datetime.now().year}")
     progress_bar["value"] = 0
     progress_bar["maximum"] = 365
-    progress_bar["value"] = day_of_year
-    window.update_idletasks()
+
+    for i in range(day_of_year+1):
+        progress_bar["value"] = i
+        print(f"Day of year: {day_of_year}")
+        print(i)
+        window.update_idletasks()
+
     if todayPercent == tomorrowPercent:
         stats.configure(text=f"{math.floor((day_of_year / 365) * 100)}% of {datetime.now().year} complete.")
     elif todayPercent != tomorrowPercent:
         stats.configure(text=f"{math.floor((day_of_year / 365) * 100)}% of {datetime.now().year} complete. Wait for tomorrow!")
 
 
-calc_button = tk.Button(window, text="Calculate", command=calculate)
+calc_button = ttk.Button(window, text="Calculate", command=calculate)
 calc_button.pack()
 calc_button.place(relx=0.5, rely=0.75, anchor="center")
 
-stats = tk.Label(window, text=f"")
+stats = ttk.Label(window, text="")
 stats.pack()
+stats.place(relx=0.5, rely=0.4, anchor="center")
 
 tk.mainloop()
